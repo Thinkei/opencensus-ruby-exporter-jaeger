ec8814439021095beab4991766d5afa8270fc8e5
Luong Vo <vo.tran.thanh.luong@gmail.com>
2019-08-06 12:55:45 +0700

    Remove internal gem publishing process

--- scripts/publish.rb
+++
@@ -1,7 +0,0 @@
-lib = File.expand_path("../lib", __FILE__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require 'opencensus/jaeger/version'
-
-version = OpenCensus::Jaeger::VERSION
-
-exec("gem build opencensus-jaeger.gemspec && curl -F package=@opencensus-jaeger-#{version}.gem https://#{ENV['GEMFURY_TOKEN']}@push.fury.io/#{ENV['GEMFURY_PACKAGE']}/")
